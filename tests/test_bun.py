2b55406f10e8d166f72be4bc811215454090eab9
Karlen <arabadzhyank@gmail.com>
2025-12-29 14:14:10 +0700

    Разделил тесты для методов Bun

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -2,16 +2,19 @@ import pytest
 from bun import Bun
 
 
-@pytest.mark.parametrize(
-    "name, price",
-    [
-        ("black bun", 100),
-        ("white bun", 200),
-        ("gluten free", 300),
-    ]
-)
-def test_bun_get_name_and_price(name, price):
-    bun = Bun(name, price)
+BUN_DATA = [
+    ("black bun", 100),
+    ("white bun", 200),
+    ("gluten free", 300),
+]
 
+@pytest.mark.parametrize("name, price", BUN_DATA)
+def test_bun_get_name(name, price):
+    bun = Bun(name, price)
     assert bun.get_name() == name
+
+
+@pytest.mark.parametrize("name, price", BUN_DATA)
+def test_bun_get_price(name, price):
+    bun = Bun(name, price)
     assert bun.get_price() == price
