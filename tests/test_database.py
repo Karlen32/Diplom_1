6b0c9e8009dffe87d29a8a7cc5c251765ea08db7
Karlen <arabadzhyank@gmail.com>
2025-12-29 14:34:31 +0700

    Переделал тесты на проверку методов приложения

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,21 +1,59 @@
 from database import Database
-from bun import Bun
-from ingredient import Ingredient
+from burger import Burger
 
 
-def test_database_available_buns():
+def test_set_buns_with_bun_from_database():
     database = Database()
-    buns = database.available_buns()
+    bun = database.available_buns()[0]
 
-    assert isinstance(buns, list)
-    assert all(isinstance(bun, Bun) for bun in buns)
-    assert len(buns) > 0
+    burger = Burger()
+    burger.set_buns(bun)
+
+    assert burger.bun == bun
 
 
-def test_database_available_ingredients():
+def test_get_price_with_bun_from_database():
     database = Database()
-    ingredients = database.available_ingredients()
+    bun = database.available_buns()[0]
 
-    assert isinstance(ingredients, list)
-    assert all(isinstance(ingredient, Ingredient) for ingredient in ingredients)
-    assert len(ingredients) > 0
+    burger = Burger()
+    burger.set_buns(bun)
+
+    assert burger.get_price() == bun.get_price() * 2
+
+
+def test_add_ingredient_from_database():
+    database = Database()
+    ingredient = database.available_ingredients()[0]
+
+    burger = Burger()
+    burger.add_ingredient(ingredient)
+
+    assert burger.ingredients == [ingredient]
+
+
+def test_get_price_with_bun_and_ingredients_from_database():
+    database = Database()
+    bun = database.available_buns()[0]
+    ingredient = database.available_ingredients()[0]
+
+    burger = Burger()
+    burger.set_buns(bun)
+    burger.add_ingredient(ingredient)
+
+    expected_price = bun.get_price() * 2 + ingredient.get_price()
+    assert burger.get_price() == expected_price
+
+
+def test_get_receipt_contains_ingredient_from_database():
+    database = Database()
+    bun = database.available_buns()[0]
+    ingredient = database.available_ingredients()[0]
+
+    burger = Burger()
+    burger.set_buns(bun)
+    burger.add_ingredient(ingredient)
+
+    receipt = burger.get_receipt()
+
+    assert ingredient.get_name() in receipt
