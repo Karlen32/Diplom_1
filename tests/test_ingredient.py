b2b294edc57251b3dd0bb04e604f24be7744d25e
Karlen <arabadzhyank@gmail.com>
2025-12-29 14:51:17 +0700

    Разделил тесты Ingredient на атомарные

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -3,16 +3,25 @@ from ingredient import Ingredient
 from ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
 
-@pytest.mark.parametrize(
-    "ingredient_type, name, price",
-    [
-        (INGREDIENT_TYPE_SAUCE, "hot sauce", 100),
-        (INGREDIENT_TYPE_FILLING, "cutlet", 200),
-    ]
-)
-def test_ingredient_getters(ingredient_type, name, price):
-    ingredient = Ingredient(ingredient_type, name, price)
+INGREDIENT_DATA = [
+    (INGREDIENT_TYPE_SAUCE, "hot sauce", 100),
+    (INGREDIENT_TYPE_FILLING, "cutlet", 200),
+]
 
+
+@pytest.mark.parametrize("ingredient_type, name, price", INGREDIENT_DATA)
+def test_ingredient_get_type(ingredient_type, name, price):
+    ingredient = Ingredient(ingredient_type, name, price)
     assert ingredient.get_type() == ingredient_type
+
+
+@pytest.mark.parametrize("ingredient_type, name, price", INGREDIENT_DATA)
+def test_ingredient_get_name(ingredient_type, name, price):
+    ingredient = Ingredient(ingredient_type, name, price)
     assert ingredient.get_name() == name
+
+
+@pytest.mark.parametrize("ingredient_type, name, price", INGREDIENT_DATA)
+def test_ingredient_get_price(ingredient_type, name, price):
+    ingredient = Ingredient(ingredient_type, name, price)
     assert ingredient.get_price() == price
